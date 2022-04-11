89060d434a78b8170ef0f836079205370bf438a1
Aline Pollis <aline.pollis@hotmail.com>
2022-04-10 23:17:30 -0300

    Estilo carrossel

--- frontend/src/app/components/frontend/servico/servico.component.ts
+++ frontend/src/app/components/frontend/servico/servico.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-servico',
@@ -8,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class ServicoComponent implements OnInit {
 
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+   }
 
   ngOnInit(): void {
   }
